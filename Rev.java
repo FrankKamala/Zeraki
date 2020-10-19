30a29bc9a7517681c4bf17cfd10237fc830d4b8c
FrankKamala <frankkamala@gmail.com>
2020-10-19 15:54:54 +0300

    adjust space

--- Rev.java
+++ Rev.java
@@ -3,8 +3,8 @@ public class TaskTwo {
         String s = "Lorem at";
         StringBuffer buf = new StringBuffer(s);
         buf.reverse();
-        System.out.println("Current string is" + s);
-        System.out.println("reversed result is" + buf);
+        System.out.println("Current string is " + s);
+        System.out.println("reversed result is " + buf);
     }
    
 } 
