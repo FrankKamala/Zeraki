5a5543912c02bf1f54b59b2243bab9c85a1b6c2e
FrankKamala <frankkamala@gmail.com>
2020-10-19 15:53:05 +0300

    task2

--- Rev.java
+++ Rev.java
@@ -1,3 +1,9 @@
 public class TaskTwo { 
+    public static void main (String []args){
+        String s = "Lorem at";
+        StringBuffer buf = new StringBuffer(s);
+        s.reverse();
+        System.out.println("reversed result is" + s)
+    }
    
 } 
