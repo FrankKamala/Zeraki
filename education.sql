a87d846fe59a6f5356420fb107666817172fcfa8
FrankKamala <frankkamala@gmail.com>
2020-10-19 16:27:09 +0300

    comment

--- education.sql
+++ education.sql
@@ -1,21 +1,23 @@
 CREATE DATABASE education;
 USE education;
-
+/* Create Students Table*/
 CREATE TABLE student(
     studentid INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(250) NOT NULL,
     course INT(11,
     FOREIGN KEY (course) REFERENCES course(courseid))
 );
-
+/* Create Course Table*/
 
 CREATE TABLE course(
     courseid INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(250) NOT NULL,institution INT(11),
     FOREIGN KEY (institution) REFERENCES institution(institutionid)
     );
-
+/* Create Institution Tabble*/
 
 CREATE TABLE institution(
     institutionid INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(250) NOT NULL);
+
+
