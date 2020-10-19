2befb9ca1f5272909e69e36ee25b60bf56d7d199
FrankKamala <frankkamala@gmail.com>
2020-10-19 16:45:16 +0300

    final

--- education.sql
+++ education.sql
@@ -12,7 +12,8 @@ CREATE TABLE student(
 
 CREATE TABLE course(
     courseid INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(250) NOT NULL,institution INT(11),
+    name VARCHAR(250) NOT NULL,
+    institution INT(11),
     FOREIGN KEY (institution) REFERENCES institution(institutionid)
     );
 /* Create Institution Tabble*/
@@ -22,3 +23,9 @@ CREATE TABLE institution(
     name VARCHAR(250) NOT NULL);
 
 
+/*To output contents*/
+SELECT institution.name as "INSTITUTIONS_NAME",course.name as "COURSE_NAME", count(student.name) as" NUMBER_OF_STUDENTS" from student
+ inner join course on student.course = course.courseid 
+ inner join institution on institution.institutionid=course.institution
+ ORDER BY COURSE_NAME DESC;
+ 
